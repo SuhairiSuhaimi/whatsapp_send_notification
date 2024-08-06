5dd5a72cadbbebb31f1b8db9be3a09fdd84e2f14
SuhairiSuhaimi <suhairi@cmg.com.my>
2024-08-06 17:29:31 +0800

    Delete

--- dist/app copy.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const venom_1 = require("./service/venom");
-const vbot = new venom_1.VenomService();
-vbot.init();
-//# sourceMappingURL=app.js.map
